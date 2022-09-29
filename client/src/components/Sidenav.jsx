ab362df03bbcdd495ea00c64cbee21f588eb28f4
Andrea <andrea.berrocal95@gmail.com>
2022-09-29 16:06:16 -0700

    added sidenav and heroicons

---
+++ client/src/components/Sidenav.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  ChatBubbleBottomCenterIcon,
+  VideoCameraIcon,
+  GlobeAltIcon,
+  MusicalNoteIcon,
+  CalendarDaysIcon,
+  Cog6ToothIcon,
+  ArrowLeftOnRectangleIcon,
+} from "@heroicons/react/24/outline";
+
+
+const ICONWIDTH = 25;
+
+function SideNav() {
+  return (
+    <div className="drawer-side">
+      <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+      <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+        <li>
+          <a>
+            <div className="avatar">
+              <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img src="https://placeimg.com/192/192/people" />
+              </div>
+            </div>
+          </a>
+        </li>
+        <hr />
+        <li>
+          <a href="">
+            <GlobeAltIcon />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <ChatBubbleBottomCenterIcon width={ICONWIDTH} />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <VideoCameraIcon width={ICONWIDTH} />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <MusicalNoteIcon width={ICONWIDTH} />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <CalendarDaysIcon width={ICONWIDTH} />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <Cog6ToothIcon width={ICONWIDTH} />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <ArrowLeftOnRectangleIcon width={ICONWIDTH} />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default SideNav;
