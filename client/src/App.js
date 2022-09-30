2d36ff8d83bf63f9b221dac5cda578b9932d461b
Andrea <andrea.berrocal95@gmail.com>
2022-09-30 12:41:34 -0700

    added login page and changed styling in other components as well

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import SideNav from "./components/Sidenav";
 import Layout from "./views/Layout";
+import Login from "./views/Login";
+import { useState } from 'react';
 
 function App() {
+  const [loggedin, setLoggedIn] = useState(true);
+
   return (
-    <Layout />
+    <>
+    {!loggedin ? <Login /> : <Layout loggedin={loggedin}/>}
+    </>
   );
 }
 
--- client/src/components/Sidenav.jsx
+++ client/src/components/Sidenav.jsx
@@ -14,13 +14,13 @@ const ICONWIDTH = 25;
 
 function SideNav() {
   return (
-    <div className="drawer-side side-nav bg-base-300">
-      <label htmlFor="my-drawer-2" className="drawer-overlay" ></label>
-      <ul className="menu p-2 overflow-y-auto w-78 bg-base-300 text-base-content">
+    <div className="drawer-side side-nav bg-teal-300">
+      <label htmlFor="my-drawer-2" className="drawer-overlay bg-teal-300" ></label>
+      <ul className="menu p-2 overflow-y-auto w-78 text-base-content">
         <li>
           <a>
             <div className="avatar">
-              <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <div className="w-10 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2">
                 <img src="https://placeimg.com/192/192/people" />
               </div>
             </div>
